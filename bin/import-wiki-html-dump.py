6650ac69baaab1c1c4af87b8c94e8b748035544a
sqs <qslack@qslack.com>
2010-06-10 21:16:55 -0700

    import-wiki-html-dump script

---
+++ bin/import-wiki-html-dump.py
@@ -0,0 +1,28 @@
+import sys, os
+from freequery.repository.repository import Repository
+from freequery.repository.document import Document
+
+if len(sys.argv) != 3:
+    print "Usage: %s <repos> <wiki>" % sys.argv[0]
+    exit(1)
+
+repospath = sys.argv[1]
+wikipath = os.path.join(sys.argv[2], 'articles')
+
+repos = Repository(repospath)
+i = 1
+
+for root, dirs, files in os.walk(wikipath):
+    for f in files:
+        if f.endswith('.html'):
+            path = os.path.join(root, f)
+            uri = 'file://' + path
+            with open(path, 'r') as ff:
+                contents = ff.read()
+            doc = Document(uri, {'orig': contents})
+            repos.add(doc)
+            print "%d. %s" % (i, uri)
+            i += 1
+
+repos.close()
+
