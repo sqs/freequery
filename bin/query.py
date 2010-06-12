f54481994de93ed92825e6944ab8db516d712a04
sqs <qslack@qslack.com>
2010-06-12 08:25:21 -0700

    query script for running single-term queries against inverted index

---
+++ bin/query.py
@@ -0,0 +1,24 @@
+import sys
+from freequery.repository.repository import Repository
+from freequery.index.inverted_index import InvertedIndex
+
+if len(sys.argv) != 4:
+    print "Usage: %s <repos> <invindex> <term>" % sys.argv[0]
+    exit(1)
+
+repospath = sys.argv[1]
+invindexpath = sys.argv[2]
+term = sys.argv[3]
+
+repos = Repository(repospath)
+invindex = InvertedIndex(invindexpath)
+
+docids = invindex.lookup(term)
+for docid in docids:
+    print repos.get(docid).uri
+
+repos.close()
+invindex.close()
+
+
+
