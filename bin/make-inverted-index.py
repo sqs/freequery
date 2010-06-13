1e5cd684ecadc34a46ccc5cce8a0a3d9df3bf690
sqs <qslack@qslack.com>
2010-06-13 16:50:55 -0700

    using InvertedIndexReader instead of old InvertedIndex

--- bin/make-inverted-index.py
+++ bin/make-inverted-index.py
@@ -1,6 +1,6 @@
 import sys
 from freequery.index.forward_index import ForwardIndex
-from freequery.index.inverted_index import InvertedIndex
+from freequery.index.inverted_index import InvertedIndexWriter
 
 if len(sys.argv) != 3:
     print "Usage: %s <fwdindex> <invindex>" % sys.argv[0]
@@ -10,7 +10,7 @@ fwdindexpath = sys.argv[1]
 invindexpath = sys.argv[2]
 
 fwdindex = ForwardIndex(fwdindexpath)
-invindex = InvertedIndex(invindexpath)
+invindex = InvertedIndexWriter(invindexpath)
 
 i = 0
 for e in fwdindex:
--- bin/query.py
+++ bin/query.py
@@ -1,6 +1,6 @@
 import sys
 from freequery.repository.repository import Repository
-from freequery.index.inverted_index import InvertedIndex
+from freequery.index.inverted_index import InvertedIndexReader
 
 if len(sys.argv) != 4:
     print "Usage: %s <repos> <invindex> <term>" % sys.argv[0]
@@ -11,9 +11,9 @@ invindexpath = sys.argv[2]
 term = sys.argv[3]
 
 repos = Repository(repospath)
-invindex = InvertedIndex(invindexpath)
+invindex = InvertedIndexReader(invindexpath)
 
-docids = map(lambda r: r[0], invindex.lookup(term))
+docids = map(lambda p: p.docid, invindex.lookup(term))
 for docid in docids:
     print repos.get(docid).uri
 
