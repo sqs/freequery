a2bbbc30862c3c8fa7bd1f9f528904b38fb3a74d
sqs <qslack@qslack.com>
2010-06-14 21:51:34 -0700

    test_document using docs fixture

--- tests/test_units/test_document.py
+++ tests/test_units/test_document.py
@@ -1,26 +1,22 @@
 import unittest
 from freequery.repository.document import Document
+from tests.fixtures import docs
 
 
 class TestDocument(unittest.TestCase):
 
     def test_eq(self):
-        assert Document('http://example.com', {'title': 'Example'}) == \
-               Document('http://example.com', {'title': 'Example'})
-        assert Document('http://example.com', {'title': 'Example'}) != \
-               Document('http://apple.com', {'title': 'Apple'})
+        assert Document('http://example.com', '<h1>Welcome to example</h1') == \
+               Document('http://example.com', '<h1>Welcome to example</h1')
+        assert Document('http://example.com', '<h1>Welcome to example</h1') != \
+               Document('http://apple.com', '<h1>Welcome to Apple</h1>')
 
     def test_pack(self):
-        d1 = Document('http://example.com', '<h1>Welcome to example</h1>')
-        proto_d1 = Document.from_proto_string(d1.to_proto_string())
-        assert d1 == proto_d1
+        proto_doc = Document.from_proto_string(docs.example.to_proto_string())
+        assert docs.example == proto_doc
 
 class TestHTMLDocument(unittest.TestCase):
 
-    def setUp(self):
-        self.d1 = Document('http://example.com', '<h1>Welcome to example</h1>')
-        self.d1.make_typed('text/html')
-
     def test_tokens(self):
-        assert ['Welcome', 'to', 'example'] == self.d1.tokens()
+        assert ['Welcome', 'to', 'example'] == docs.example.tokens()
         
