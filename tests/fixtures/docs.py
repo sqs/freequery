ac61904864965bbef576c8cefcd02e3afd931edf
sqs <qslack@qslack.com>
2010-06-14 21:44:45 -0700

    document fixtures

---
+++ tests/fixtures/docs.py
@@ -0,0 +1,4 @@
+from freequery.repository.document import Document
+
+example = Document('http://example.com', '<h1>Welcome to example</h1>')
+apple = Document('http://apple.com', '<h1>Welcome to Apple</h1>')
