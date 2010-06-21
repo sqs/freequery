25deda5a887dcb73c27f7b215d0ed7ea16f7e67d
sqs <qslack@qslack.com>
2010-06-21 15:28:14 -0700

    script for converting a static html dump directory of wikipedia to a qtable dump file

---
+++ bin/convert-static-wiki-dump.py
@@ -0,0 +1,22 @@
+import os, sys
+from freequery.document import Document
+from freequery.repository.formats import QTableFileWriter
+
+if len(sys.argv) != 2:
+    print "Usage: %s <wiki-path>" % sys.argv[0]
+    exit(1)
+
+    
+wikipath = os.path.join(sys.argv[1], 'articles')
+
+dump_writer = QTableFileWriter(sys.stdout)
+
+for root, dirs, files in os.walk(wikipath):
+    for f in files:
+        if f.endswith('.html'):
+            path = os.path.join(root, f)
+            with open(path, 'rb') as ff:
+                raw = ff.read()
+            doc = Document(path, raw)
+            dump_writer.write(doc)
+            sys.stderr.write(path + "\n")
