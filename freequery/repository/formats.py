7bf03187d7efa26541233ac45401bbbaeb5f5eec
sqs <qslack@qslack.com>
2010-06-21 14:53:02 -0700

    QTableFileWriter

--- freequery/repository/formats.py
+++ freequery/repository/formats.py
@@ -30,7 +30,27 @@ class QTableFile(object):
                 raw.append(line)
             else: # doc finished
                 break
+
+        # remove "\n" from last line of raw, since it delimits
+        # the raw from the end-of-document and is not actually part
+        # of the raw content
+        if len(raw) > 0:
+            raw[-1] = raw[-1].rstrip()
+            
         return Document(uri, ''.join(raw))
 
     def __iter__(self):
         return self
+
+class QTableFileWriter(object):
+
+    """Writer for QTable dump files."""
+    
+    def __init__(self, out):
+        self.out = out
+
+    def write(self, doc):
+        """Writes `doc` to the output stream."""
+        self.out.writelines((doc.uri, "\n\n", doc.raw, "\n", QTableFile.DELIM))
+
+    
--- tests/test_units/test_formats.py
+++ tests/test_units/test_formats.py
@@ -1,16 +1,26 @@
-import unittest
-from freequery.repository.formats import QTableFile
-from freequery.test.fixtures import qtable_file1
+import unittest, StringIO
+from freequery.repository.formats import QTableFile, QTableFileWriter
+from freequery.test import fixtures
 
 class TestQTableFile(unittest.TestCase):
 
     def test_parses_file1(self):
-        parser = QTableFile(qtable_file1.splitlines(True))
+        parser = QTableFile(fixtures.qtable_file1.splitlines(True))
         example = parser.next()
         apple = parser.next()
         self.assertRaises(StopIteration, parser.next)
         self.assertEquals("http://example.com", example.uri)
         self.assertEquals("http://apple.com", apple.uri)
-        self.assertEquals("<h1>Welcome to example</h1>\n", example.raw)
+        self.assertEquals("<h1>Welcome to example</h1>", example.raw)
         self.assertEquals("<h1>Welcome to Apple</h1>", apple.raw)
 
+class TestQTableFileWriter(unittest.TestCase):
+
+    def test_writes_file1(self):
+        out = StringIO.StringIO()
+        writer = QTableFileWriter(out)
+        writer.write(fixtures.example)
+        writer.write(fixtures.apple)
+        parser = QTableFile(out.getvalue().splitlines(True))
+        self.assertEquals(fixtures.example, parser.next())
+        self.assertEquals(fixtures.apple, parser.next())
