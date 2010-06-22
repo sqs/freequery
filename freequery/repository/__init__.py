3faa786c484ba270265024bb83130212f562aeac
sqs <qslack@qslack.com>
2010-06-21 20:06:05 -0700

    removed old, non-mapreduce code

--- freequery/repository/__init__.py
+++
@@ -1,2 +0,0 @@
-from freequery.repository.repository import Repository
-from freequery.repository.document_index import DocumentIndex, rptr
--- tests/test_integration/tests.py
+++ tests/test_integration/tests.py
@@ -1,27 +1,9 @@
 from freequery.test import IntegrationTestCase, fixtures as docs
-from freequery.repository.repository import Repository
-from freequery.index.forward_index import ForwardIndex
-from freequery.index.inverted_index import InvertedIndexWriter, InvertedIndexReader
-
-TEST_REPOSITORY_PATH = "/tmp/fq-test/"
 
 class TestIntegration(IntegrationTestCase):
 
     def __run_test(self, docs, query, expected_result_uris):
-        repos = Repository(TEST_REPOSITORY_PATH)
-        fwdindex = ForwardIndex(TEST_REPOSITORY_PATH + 'fwdindex')
-        iiwriter = InvertedIndexWriter(TEST_REPOSITORY_PATH + 'invindex')
-        for doc in docs:
-            docid = repos.add(doc)
-            doc.docid = docid
-            fwdindex.add(doc)            
-        
-        for e in fwdindex:
-            iiwriter.add(e)
-        iiwriter.finish()
-        fwdindex.close()
-
-        iireader = InvertedIndexReader(TEST_REPOSITORY_PATH + 'invindex')
+	assert False
         result_uris = [repos.get(e.docid).uri for e in iireader.lookup(query)]
         repos.close()
         self.assertEquals(list(expected_result_uris), result_uris)
