b1e1becc21884179e9db312a237edc8cda4415cc
sqs <qslack@qslack.com>
2010-06-21 19:59:38 -0700

    delay after starting index job so that clone finds old name

--- bin/index.py
+++ bin/index.py
@@ -2,7 +2,7 @@
 Indexes a Web dump file given as a command-line argument.
 """
 
-import sys, commands
+import sys, commands, time
 
 if len(sys.argv) != 2:
     print "Usage: %s <dump>" % sys.argv[0]
@@ -11,6 +11,8 @@ if len(sys.argv) != 2:
 index_id = commands.getoutput(
     "echo '%s' | discodex index --parser freequery.index.mapreduce.docparse " \
     "--demuxer freequery.index.mapreduce.docdemux " \
-    % sys.argv[1])
+    % sys.argv[1]).strip()
 
+print index_id
+time.sleep(1)
 print commands.getoutput("discodex clone %s fq" % index_id)
