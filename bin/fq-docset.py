1e966fa372e42cc5c778b390d6ea2996876dfecf
sqs <qslack@qslack.com>
2010-06-30 22:55:28 -0700

    implemented fq-docset commands

--- bin/fq-docset.py
+++ bin/fq-docset.py
@@ -14,24 +14,38 @@ class FreequeryDocset(Program):
         return dict((k, v) for k, v in self.options.__dict__.iteritems()
                            if v is not None)
 
+    @property
+    def ddfs(self):
+        from disco.ddfs import DDFS
+        return DDFS()
+
+    @property
+    def docset(self):
+        from freequery.repository.docset import Docset
+        return Docset
+    
     def default(self, program, *args):
         ls(program)
 
 @FreequeryDocset.command
-def ls(program):
-    """Usage:
+def ls(program, docsetname=None):
+    """Usage: [docsetname]
 
-    Prints the names of all docsets.
+    Prints the names of all docsets, or of the dumps in the specified docset.
     """
-    pass
-    
-@FreequeryDocset.command
-def new(program, docsetname):
-    """Usage: <docsetname>
-
-    Creates a new docset named `docsetname`.
-    """
-    pass
+    if docsetname is None:
+        docsets = program.ddfs.list('fq:docset:')
+        if docsets:
+            print "\n".join(docsets)
+    else:
+        docset = program.docset(docsetname)
+        if docset.exists():
+            dumps = docset.dump_names()
+            if dumps:
+                print "\n".join(dumps)
+        else:
+            print "fq-docset: cannot access `%s': no such docset" % docsetname
+            exit(1)
 
 @FreequeryDocset.command
 def rm(program, docsetname):
@@ -39,7 +53,12 @@ def rm(program, docsetname):
 
     Deletes the specified docset.
     """
-    pass
+    docset = program.docset(docsetname)
+    if docset.exists():
+        program.docset(docsetname).delete()
+    else:
+        print "fq-docset: cannot remove `%s': no such docset" % docsetname
+        exit(1)
 
 @FreequeryDocset.command
 def add(program, docsetname, dump):
@@ -47,7 +66,9 @@ def add(program, docsetname, dump):
 
     Adds the dumpfile `dump` to the specified docset.
     """
-    pass
+    docset = program.docset(docsetname)
+    dumpname = os.path.basename(dump)
+    docset.add_dump(dumpname, dump)
 
 @FreequeryDocset.command
 def info(program, docsetname):
