b2f9489d2da201f4586d2e509566cf979a1b8cb5
sqs <qslack@qslack.com>
2010-07-03 11:25:22 -0700

    test for search button label

--- freequery/org/search/tests.py
+++ freequery/org/search/tests.py
@@ -3,6 +3,6 @@ from django.test import TestCase
 class TestHome(TestCase):
     def test_home(self):
         res = self.client.get('/')
-        self.assertContains(res, 'Freequery')
+        self.assertContains(res, 'Freequery Search')
         
 
