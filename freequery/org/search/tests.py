985efdc786fea824046f2bcd8d83b42ecc60b20a
sqs <qslack@qslack.com>
2010-07-03 11:34:24 -0700

    results page stub

--- freequery/org/search/tests.py
+++ freequery/org/search/tests.py
@@ -4,5 +4,20 @@ class TestHome(TestCase):
     def test_home(self):
         res = self.client.get('/')
         self.assertContains(res, 'Freequery Search')
-        
 
+class TestResults(TestCase):
+    def test_results_q_missing(self):
+        res = self.client.get('/search')
+        self.assertRedirects(res, '/')
+
+    def test_results_q_empty(self):
+        res = self.client.get('/search?q=')
+        self.assertRedirects(res, '/')
+
+    def test_results_q_whitespace(self):
+        res = self.client.get('/search?q=%20%20')
+        self.assertRedirects(res, '/')
+
+    def test_results_q_in_title(self):
+        res = self.client.get('/search?q=abc123')
+        self.assertContains(res, '<title>abc123 - Freequery</title>')
--- freequery/org/search/views.py
+++ freequery/org/search/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 
 def home(req):
     return render_to_response('search/home.html')
+
+def search(req):
+    q = req.GET.get('q', None)
+    if not q or q.isspace():
+        return HttpResponseRedirect('/')
+    return render_to_response('search/results.html', {'q': q})
