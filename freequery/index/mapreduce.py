a60e4f98a658ca00c411c0688fc1e287e825e7d9
sqs <qslack@qslack.com>
2010-07-03 17:52:27 -0700

    force key (term) to str

--- freequery/index/mapreduce.py
+++ freequery/index/mapreduce.py
@@ -13,6 +13,9 @@ def docdemux(doc, params):
     tf)`, where `tf` is the term frequency in this document (uses in-mapper
     combining to calculate these here)."""
     for term in set(doc.terms()):
-        yield term.decode('utf8'), doc.uri
+        t = term.decode('utf8')
+        t = str(t)
+        assert isinstance(t, str)
+        yield t, doc.uri
 
 
