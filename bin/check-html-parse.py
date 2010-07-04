91d07a5b2f9034cccc5f798362699eba8213a4da
sqs <qslack@qslack.com>
2010-07-03 18:16:48 -0700

    dont print links to save time

--- bin/check-html-parse.py
+++ bin/check-html-parse.py
@@ -4,4 +4,4 @@ from freequery.repository.formats import QTableFile
 for doc in QTableFile(open(sys.argv[1], 'rb')):
     print doc.uri
     for uri in doc.link_uris():
-        print " - %s" % uri
+        pass#print " - %s" % uri
