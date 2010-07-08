4a9350f2896f0e4cff71ab10f1adfb1a9fdac82a
sqs <qslack@qslack.com>
2010-07-08 08:14:24 -0700

    added docstrings to Docset

--- freequery/repository/docset.py
+++ freequery/repository/docset.py
@@ -4,26 +4,48 @@ from disco.util import urlresolve
 from freequery.repository.formats import QTableFile
 
 class Docset(object):
+    """
+    A `Docset` represents a set of documents, contained in dump files stored on
+    DDFS.
+    """
 
     def __init__(self, docset_name):
+        """
+        Creates a new Docset class. Class instantiation alone doesn't do
+        anything to DDFS; the docset tag won't exist until a dump is added.
+        """
         self.ddfs_tag = docset_name
         self.ddfs = DDFS()
 
     def exists(self):
+        """Returns True if this Docset exists in DDFS."""
         return self.ddfs.exists(self.ddfs_tag)
         
     def delete(self):
-        return self.ddfs.delete(self.ddfs_tag)
+        """
+        Deletes this tag from DDFS. DDFS garbage collection will soon take care
+        of dumps in this docset with no other tags. If other docsets link to
+        this docset's dumps, then those dumps will remain.
+        """
+        self.ddfs.delete(self.ddfs_tag)
 
     NDOCS_SUFFIX = '_ndocs'
     NDOCS_RE = re.compile(r'^(?P<name>[\w\d.:_\-]+)_ndocs(?P<ndocs>\d+)$')
     def add_dump(self, dumpname, dump):
+        """
+        Adds a dump to this docset, appending the doc count to the name of the
+        dump and then uploading the dump to DDFS with the tag for this docset.
+        """
         if self.NDOCS_SUFFIX not in dumpname:
             dumpname += "%s%d" % \
                 (self.NDOCS_SUFFIX, self.__doc_count_in_dump(dump))
         return self.ddfs.push(self.ddfs_tag, [(dump, dumpname)])
 
     def doc_count(self):
+        """
+        Returns the total number of documents contained in all dumps in this
+        docset.
+        """
         return sum(int(self.NDOCS_RE.match(s).group('ndocs')) \
                        for s in self.dump_names())
 
@@ -32,6 +54,10 @@ class Docset(object):
             return sum(1 for doc in QTableFile(f))
 
     def dump_uris(self):
+        """
+        Returns disco:// URIs for each dump in the docset. Use
+        disco.util.urlresolve to convert the disco:// URIs to http:// URIs.
+        """
         return (uri for (uri,) in self.ddfs.blobs(self.ddfs_tag))
     
     def __blob_uri_to_dump_name(self, bloburi):
@@ -43,9 +69,11 @@ class Docset(object):
         return re.search(r'/([\w0-9_\-@:]+)\$', bloburi).group(1)
     
     def dump_names(self):
+        """Returns the names, including doc counts, of dumps in the docset."""
         return [self.__blob_uri_to_dump_name(uri) for uri in self.dump_uris()]
 
     def dump_names_without_doc_counts(self):
+        """Returns the names, without doc counts, of dumps in the docset."""
         return [self.NDOCS_RE.match(s).group('name') for s in self.dump_names()]
     
     def get(self, uri):
