98501dcf75d793761df4a107bffedbdee85c68b9
sqs <qslack@qslack.com>
2010-07-21 19:08:14 -0700

    write-warc script

---
+++ bin/write-warc.py
@@ -0,0 +1,11 @@
+import sys
+from freequery.document import Document
+from freequery.repository.formats import WARCWriter
+
+if len(sys.argv) != 2:
+    print "usage: %s <uri>" % sys.argv[0]
+    exit(1)
+
+doc = Document(sys.argv[1], sys.stdin.read())
+writer = WARCWriter(sys.stdout)
+writer.write(doc)
