5804aa141fc545bf30a6337a1f7943614d0df78a
sqs <qslack@qslack.com>
2010-07-24 16:36:43 -0700

    doc_term_map

--- freequery/index/mapreduce.py
+++ freequery/index/mapreduce.py
@@ -7,6 +7,29 @@ def docparse(iterable, size, fname, params):
     from freequery.repository.formats import WARCParser
     return WARCParser(iterable)
 
+def doc_term_map(doc, params):
+    """
+    For each term `t` in `doc`, emits `(t,1)` to track df (document frequency)
+    and `(t, (d,tf))` to track tf (term frequency).
+    """
+    for t in set(doc.terms()):
+        yield t, 1
+
+    for t, tf in doc.term_frequencies().items():
+        yield t, (doc, tf)
+
+#def doc_term_partition(key, nr_partitions, params):
+#    """
+#    Ensures entries with the same key (term) are partitioned together.
+#    """
+#    return hash(key) % nr_partitions
+    
+def doc_term_reduce(in_iter, out, params):
+    """
+    Assumes that `(t,1)`'s (to track df) are sorted before `(t,
+    (d,tf))`'s. Emits `(t, (d,tf-idf))`.
+    """
+
 def docdemux(doc, params):
     """Emits (term, docuri) for each term in `doc`."""
     tfs = ((k.encode('utf8'), v) for k,v in doc.term_frequencies().items())
--- test/unit/test_mapreduce.py
+++ test/unit/test_mapreduce.py
@@ -1,19 +1,29 @@
 import unittest
-from freequery.index import mapreduce
+from freequery.index.mapreduce import *
 from freequery.test import fixtures
+from freequery.document import Document
 
 
 class TestDocparse(unittest.TestCase):
 
     def test_docparse(self):
         from StringIO import StringIO
-        o = mapreduce.docparse(StringIO(fixtures.warc_file1), None, None, None)
+        o = docparse(StringIO(fixtures.warc_file1), None, None, None)
         self.assertEquals([fixtures.example, fixtures.apple], list(o))
 
 class TestDocdemux(unittest.TestCase):
     def test_docdemux(self):
         o = []
         for d in (fixtures.example, fixtures.apple):
-            o.extend(list(mapreduce.docdemux(d, None)))
+            o.extend(list(docdemux(d, None)))
         exp = [('welcom', 'http://example.com'), ('exampl', 'http://example.com'), ('welcom', 'http://apple.com'), ('appl', 'http://apple.com')]
         self.assertEquals(sorted(exp), sorted(o))
+
+class TestMapReduce(unittest.TestCase):
+
+    def test_doc_term_map(self):
+        d = Document('http://a.com', 'hello there hello')
+        out = list(doc_term_map(d, None))
+        expected = [('hello', 1), ('there', 1), ('hello', (d, 2)),
+                    ('there', (d, 1))]
+        self.assertEqual(sorted(expected), sorted(out))
