4948673dbe0cbf1db6994707b545862762d769ac
sqs <qslack@qslack.com>
2010-07-24 16:35:33 -0700

    only try to instantiate html parser

--- bin/check-html-parse.py
+++ bin/check-html-parse.py
@@ -7,6 +7,7 @@ for path in inpaths:
     print "@ %s" % path
     for doc in WARCParser(open(path, 'rb')):
         print doc.uri
-        doc.links_lxml_html()
-        for uri in doc.link_uris:
-            pass#print " - %s" % uri
+        doc.html_parser
+        #doc.links_lxml_html()
+        #for uri in doc.link_uris:
+        #    pass#print " - %s" % uri
